365dabe4282a35de437537d5a41ba90b9619126c
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-04-15 22:38:27 -0500

    completed through ch. 3

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -25,3 +25,26 @@ ANIMALS = [
 
 def get_all_animals():
     return ANIMALS
+
+# Function with a single parameter
+
+
+def get_single_animal(id):
+    # Variable to hold the found animal, if it exists
+    requested_animal = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for animal in ANIMALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if animal["id"] == id:
+            requested_animal = animal
+
+    return requested_animal
+
+
+print(get_single_animal(2))
+
+# A. get_single_animal relies on each animal having a UNIQUE id. otherwise it will go in order (not stopping) and overwrite requested_animal every time it finds a match
+# it also relies on each list item containing a dict with a value of id. if an iterable does not satisfy that, python throws a key error. b/c python tries to access animal["id"] when "id" key doesn't exist?!??
