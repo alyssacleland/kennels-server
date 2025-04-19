7fcd6201dcd3434f99d19705a7d522cc391d5dee
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-04-18 23:54:59 -0500

    did chapter 4- creating/put (post in postman)! YAY SO COOL

--- views/employee_requests.py
+++ views/employee_requests.py
@@ -16,3 +16,11 @@ def get_single_employee(id):
         if employee["id"] == id:
             requested_employee = employee
     return requested_employee
+
+
+def create_employee(employee):
+    max_id = EMPLOYEES[-1]["id"]  # get id of last employee in list
+    new_id = max_id + 1
+    employee["id"] = new_id
+    EMPLOYEES.append(employee)
+    return employee  # this, paired with "self.wfile.write(json.dumps(new_animal).encode())" in request handler, is what outputs the response body (the employee json!) in postman. creating still works without them. w/o this return employee line, it outputs null (bc json dumps in request handler would be receiving None from this by default!). without the line in request handler, postman output would be an empty response body!!!
